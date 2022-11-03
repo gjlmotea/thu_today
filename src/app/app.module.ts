e069fa24b5a418878274c095f23a6940b153bbdf
gjlmotea <gjlmotea@gmail.com>
2022-11-04 02:11:44 +0800

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { CalcResultComponent } from './calc-result/calc-result.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
@@ -8,14 +8,28 @@ import { FormGroup } from '@angular/forms';
 })
 export class UserInputComponent implements OnInit {
 
-  formGroup = new FormGroup({
+  myForm: FormGroup;
 
-  })
-
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.myForm = fb.group({
+      age: 25,
+      ageLimit: 125,
+      moneyNow: 3000000,
+      moneyRate: 7, // return rate
+      spendAmount: 22000,
+      spendRate: 3, // inflation rate
+      incomeAmount: 5000,
+      incomeRate: 5,
+      ageWorkStart: 30,
+      ageWorkEnd: 50,
+    });
+  }
 
   ngOnInit(): void {
+    console.log(this.myForm.valueChanges)
+  }
 
+  formChanged() {
   }
 
 }
